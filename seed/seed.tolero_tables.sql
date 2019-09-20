811366e55794c50b89c4a6a4283825f25af7ee78
Julianb12 <julianb1212@gmail.com>
2019-09-20 15:35:25 -0500

    seed

--- seed/seed.tolero_tables.sql
+++ seed/seed.tolero_tables.sql
@@ -4,7 +4,7 @@ TRUNCATE
   posts
 RESTART IDENTITY CASCADE;
 
-INSERT INTO posts (id, content)
+INSERT INTO posts (content)
 VALUES
   ('◒ Lorem ipsum porro quisquam est qui dolorem. ◟4◞ ◓ ▲ 
   ◑ ipsum quia dolor sit amet, consectetur, adipisci velit. ◜2◝ ◐ #Lorem #Tolero');
